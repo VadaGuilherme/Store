6958761ae71a886d3bf6ddf5c8c950953b8ed710
Guilherme Vada <gvadaguariba@gmail.com>
2020-03-06 00:01:19 -0300

    Implementando a entidade de pagamento

--- Store.Tests/UnitTest1.cs
+++ Store.Tests/UnitTest1.cs
@@ -9,9 +9,7 @@ namespace Store.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var c = new Customer("Guilherme", "Vada", "123456", "gvadaguariba@gmail.com", "16993004623", "Rua Bento Carlos Botelho do Amaral");
             
-            var order = new Order(c);
         }
     }
 }
