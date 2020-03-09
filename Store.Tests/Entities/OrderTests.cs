d780c9f5a26e05e942fb6ea6bf1926a7908f6bf1
Guilherme Vada <guilherme.vada@programmers.com.br>
2020-03-09 08:01:11 -0300

    Criado os cenarios de tests da entidade de order

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,71 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Store.Tests.Entities
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void ShouldCreateOrderWhenValid()
+        {
+
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void StatusShouldBeCreatedWhenOrderCreated()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnTwoWhenAddedTwoValidItems()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnFiveWhenAddedPurchasedFiveItem()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnANumberWhenOrderPlaced()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldReturnPaidWhenOrderPaid()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldTwoWhenPurchasedTenProducts()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void StatusShouldBeCanceledWhenOrderCanceled()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void StatusCancelShippingsWhenOrderCanceled()
+        {
+            
+            Assert.Fail();
+        }
+    }
+}
