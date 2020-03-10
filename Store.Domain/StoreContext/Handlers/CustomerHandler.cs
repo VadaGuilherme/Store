e5759c57cf0d252dceb1b19be2f7ae1bf7ef1115
Guilherme Vada <gvadaguariba@gmail.com>
2020-03-09 21:29:59 -0300

    Aplicanto injecao de dependencia

--- Store.Domain/StoreContext/Handlers/CustomerHandler.cs
+++ Store.Domain/StoreContext/Handlers/CustomerHandler.cs
@@ -3,6 +3,8 @@ using FluentValidator;
 using Store.Domain.StoreContext.Commands.CustomerCommands.RequestCommands;
 using Store.Domain.StoreContext.Commands.CustomerCommands.ResponseCommands;
 using Store.Domain.StoreContext.Entities;
+using Store.Domain.StoreContext.Repositories;
+using Store.Domain.StoreContext.Services;
 using Store.Domain.StoreContext.ValueObjects;
 using Store.Shared.Commands;
 
@@ -10,8 +12,23 @@ namespace Store.Domain.StoreContext.Handlers
 {
     public class CustomerHandler : Notifiable, ICommandHandler<CreateCustomerCommandRequest>, ICommandHandler<AddAddressCommandRequest>
     {
+        private readonly ICustomerRepository _repository;
+        private readonly IEmailService _emailService;
+
+        public CustomerHandler(ICustomerRepository repository, IEmailService emailService)
+        {
+            _repository = repository;
+            _emailService = emailService;
+        }
+
         public ICommandResult Handle(CreateCustomerCommandRequest command)
         {
+            if(_repository.CheckDocument(command.Document))
+                AddNotification("Document", "Este CPF já está em uso.");
+
+            if(_repository.CheckEmail(command.Document))
+                AddNotification("Email", "Este E-mail já está em uso.");
+
             var name = new Name(command.FirstName, command.LastName);
             var document = new Document(command.Document);
             var email = new Email(command.Email);
@@ -23,7 +40,14 @@ namespace Store.Domain.StoreContext.Handlers
             AddNotifications(email.Notifications);
             AddNotifications(customer.Notifications);
 
-            return new CreateCustomerCommandResponse(Guid.NewGuid(), name.ToString(), email.Address);
+            if(Invalid)
+                return null;
+
+            _repository.Save(customer);
+
+            _emailService.Send(email.Address, "gvadaguariba@gmail.com", "Bem vindo", "Seja bem vindo");
+
+            return new CreateCustomerCommandResponse(customer.Id, name.ToString(), email.Address);
         }
 
         public ICommandResult Handle(AddAddressCommandRequest command)
