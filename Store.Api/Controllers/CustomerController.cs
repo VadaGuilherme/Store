df708c0554b563e6c612a8ddb00e60c0e9821e7c
Guilherme Vada <gvadaguariba@gmail.com>
2020-03-11 20:37:15 -0300

    Implementacao basica do CustomerController

--- Store.Api/Controllers/CustomerController.cs
+++ Store.Api/Controllers/CustomerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Store.Domain.StoreContext.Entities;
@@ -7,8 +8,43 @@ namespace Store.Api.Controllers
     public class CustomerController : Controller
     {
         [HttpGet]
-        [Route("")]
+        [Route("customers")]
         public List<Customer> Get()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}")]
+        public Customer GetById(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}/orders")]
+        public List<Order> GetOrders(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public Customer Delete()
         {
             return null;
         }
