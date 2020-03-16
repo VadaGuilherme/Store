7a3d10011e9fa1d63812775f718e744524377cc0
Guilherme Vada <gvadaguariba@gmail.com>
2020-03-15 23:48:49 -0300

    Configurando o ElmahIo Controle de LOGs

--- Store.Api/Startup.cs
+++ Store.Api/Startup.cs
@@ -1,7 +1,11 @@
+using System;
+using Elmah.Io;
+using Elmah.Io.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Store.Domain.StoreContext.Handlers;
 using Store.Domain.StoreContext.Repositories;
 using Store.Domain.StoreContext.Services;
@@ -25,10 +29,16 @@ namespace Store.Api
             services.AddTransient<CustomerHandler, CustomerHandler>();
 
             services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo {
+                c.SwaggerDoc("v1", new OpenApiInfo {
                     Title = "Store",
                     Version = "v1"
                 });
+            });
+
+            services.AddElmahIo(o =>
+{
+                o.ApiKey = "ed497fc444eb422fb374e481d05264c4";
+                o.LogId = new Guid("a91e9f2a-f4e4-474f-8438-14501b71b6eb");
             });
         }
 
@@ -49,6 +59,8 @@ namespace Store.Api
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseElmahIo();
         }
     }
 }
